0286bc4108ed7ea572a6d4e332f57d96cf2b4261
André Nunes <hello@anuness.dev>
2024-03-17 01:59:28 +0000

    feat: add vercel speed insights

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
   <NuxtLayout>
+    <SpeedInsights />
     <NuxtPage />
   </NuxtLayout>
 </template>
