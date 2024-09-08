7b8935599810ec37fd9897aaa0dfdc1f3f8b36aa
André Nunes <hello@anuness.dev>
2024-09-08 19:48:10 +0100

    fix: add logo from gravatar

--- composables/usePublicImages.ts
+++ composables/usePublicImages.ts
@@ -1,7 +1,7 @@
 export default function () {
     return useState('publicImages', () => {
         return {
-            'logo': 'https://s3.anuness.dev/assets/logo/5fb69c6ebf05b7c995ef2ccaedd6e2f7.jpg',
+            'logo': 'https://www.gravatar.com/avatar/a72c3628b6a1854a8d81283d9d0ec64c34d402728dcf11ce407aeef932a0a26f',
         };
     });
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { createHash } from 'crypto';
 import useLoading from "~/composables/useLoading";
 
 const pages = usePages();
